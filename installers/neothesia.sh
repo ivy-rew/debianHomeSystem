69880bc912a6e99cbe9cf8b518fc40502cfbe75f
LordEagle <r.wermelinger@gmail.com>
2021-08-19 19:14:32 +0200

    vulcan deps + downgrad to compatible 0.0.9

--- installers/neothesia.sh
+++ installers/neothesia.sh
@@ -4,11 +4,14 @@
 
 #Neothesia
 neoZip=/tmp/neothesia.zip
-wget -v -O ${neoZip} https://github.com/PolyMeilex/Neothesia/releases/download/v0.0.10/ubuntu-artifact.zip
+wget -v -O ${neoZip} https://github.com/PolyMeilex/Neothesia/releases/download/v0.0.9/ubuntu-artifact.zip
 unzip ${neoZip} -d /tmp
 sudo unzip /tmp/app.zip -d ${pianoDir}/neothesia
 rm ${neoZip}
 rm /tmp/app.zip
+
+#Vulcan drivers
+sudo apt install -y mesa-vulkan-drivers vulkan-utils
 
 #Font
 font=${fontDir}/neothesia-default.sf2
