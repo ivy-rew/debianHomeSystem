f62b3b0f84aff4a2172b4eafb83b718619294777
LordEagle <r.wermelinger@gmail.com>
2021-08-19 19:27:48 +0200

    link default font to play

--- installers/neothesia.sh
+++ installers/neothesia.sh
@@ -3,10 +3,11 @@
 . common.sh
 
 #Neothesia
+neoDir=${pianoDir}/neothesia
 neoZip=/tmp/neothesia.zip
 wget -v -O ${neoZip} https://github.com/PolyMeilex/Neothesia/releases/download/v0.0.9/ubuntu-artifact.zip
 unzip ${neoZip} -d /tmp
-sudo unzip /tmp/app.zip -d ${pianoDir}/neothesia
+sudo unzip /tmp/app.zip -d ${neoDir}
 rm ${neoZip}
 rm /tmp/app.zip
 
@@ -16,4 +17,4 @@ sudo apt install -y mesa-vulkan-drivers vulkan-utils
 #Font
 font=${fontDir}/neothesia-default.sf2
 sudo wget -O ${font} https://github.com/PolyMeilex/Neothesia/raw/master/default.sf2
-ln -s ${font}/ $HOME/.config/neothesia/default.sf2
+sudo ln -s ${font}/ ${neoDir}/default.sf2
