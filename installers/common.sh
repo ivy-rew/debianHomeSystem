98096b32ee649eb8ec74b308434e96a9d26647f1
LordEagle <r.wermelinger@gmail.com>
2021-08-19 18:30:35 +0200

    refactor: common settings + sudo mv

---
+++ installers/common.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pianoDir=/opt/piano
+fontDir=${pianoDir}/fonts
+
+sudo mkdir -v -p ${fontDir}
--- installers/piano.sh
+++ installers/piano.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-pianoDir=/opt/piano
-sudo mkdir ${pianoDir}
-cd ${pianoDir}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+. $DIR/common.sh
 
 #LMMS
-sudo wget https://github.com/LMMS/lmms/releases/download/v1.2.2/lmms-1.2.2-linux-x86_64.AppImage
+wget https://github.com/LMMS/lmms/releases/download/v1.2.2/lmms-1.2.2-linux-x86_64.AppImage
 sudo cp lmms.sh.template ${pianoDir}
+sudo mv -v lmms*.AppImage ${pianoDir}
 
 #PianoBooster
-sudo wget https://github.com/pianobooster/PianoBooster/releases/download/v1.0.0/PianoBooster-1.0.0-x86_64.AppImage
+wget https://github.com/pianobooster/PianoBooster/releases/download/v1.0.0/PianoBooster-1.0.0-x86_64.AppImage
+sudo mv -v PianoBooster*.AppImage ${pianoDir}
 
 #Common
-sudo wget https://github.com/pianobooster/fluid-soundfont/releases/download/v3.1/FluidR3_GM.sf2
+wget https://github.com/pianobooster/fluid-soundfont/releases/download/v3.1/FluidR3_GM.sf2
+sudo mv -v FluidR3_GM.sf2 ${fontDir}
