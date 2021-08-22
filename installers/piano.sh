34bd3286a6e37957aced9d6b71f333bb67d53daf
LordEagle <r.wermelinger@gmail.com>
2021-08-22 16:32:09 +0200

    fix overwrite + exec binaries

--- installers/piano.sh
+++ installers/piano.sh
@@ -6,16 +6,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #LMMS
 wget https://github.com/LMMS/lmms/releases/download/v1.2.2/lmms-1.2.2-linux-x86_64.AppImage
-sudo cp lmms.sh.template ${pianoDir}
-sudo mv -v lmms*.AppImage ${pianoDir}
+chmod +x lmms*.AppImage
+sudo cp -v lmms.sh.template ${pianoDir}
+sudo mv -v -f lmms*.AppImage ${pianoDir}
 
 #PianoBooster
 wget https://github.com/pianobooster/PianoBooster/releases/download/v1.0.0/PianoBooster-1.0.0-x86_64.AppImage
-sudo mv -v PianoBooster*.AppImage ${pianoDir}
+chmod +x PianoBooster*.AppImage
+sudo mv -v -f PianoBooster*.AppImage ${pianoDir}
 
 #Common
 wget https://github.com/pianobooster/fluid-soundfont/releases/download/v3.1/FluidR3_GM.sf2
-sudo mv -v FluidR3_GM.sf2 ${fontDir}
+sudo mv -v -f FluidR3_GM.sf2 ${fontDir}
 
 #Composites
 $DIR/neothesia.sh
