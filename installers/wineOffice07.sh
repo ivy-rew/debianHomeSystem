1c7a350a1ae17ee023e25cd107073b9c17ff7429
LordEagle <r.wermelinger@gmail.com>
2025-03-12 19:39:31 +0100

    office07

---
+++ installers/wineOffice07.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# https://linux.how2shout.com/how-to-install-microsoft-office-2007-on-ubuntu-20-04-lts/
+
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key\
+ https://dl.winehq.org/wine-builds/winehq.key
+
+ubu="noble"
+sudo wget -NP /etc/apt/sources.list.d/\
+ https://dl.winehq.org/wine-builds/ubuntu/dists/${noble}/winehq-noble.sources
+
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install --install-recommends winehq-stable
+
+echo "==manual step==:: ready to run ms-office installer"
+
